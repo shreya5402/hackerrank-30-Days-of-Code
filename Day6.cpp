43042b7452ad8d98ecc839121ae64384fc4a5ee1
Shreya Pandey <74672603+shreya5402@users.noreply.github.com>
2022-10-04 15:36:39 +0530

    Update Day6.cpp

--- Day6.cpp
+++ Day6.cpp
@@ -8,38 +8,34 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    int n,l,r;
-    string s,s1;
-    vector<string> v;
-    cin>>n;
-       for (int i=0;i<n;i++) {
-            cin>>s; 
-            v.push_back(s); 
-      }
-        for (int i=0;i<n;i++) {
-            s1=v[i];
-           l= s1.length();
-           
-           for (int i=0;i<l;i++) {
-                 r=i%2;
-                 
-                 if (r==0) { //even
-                    cout<<s1[i];
-                 }
-                    
-            }
-            cout<<" ";
-            for (int i=0;i<l;i++) {
-                 r=i%2;
-                 
-                 if (r==1) { //odd
-                    cout<<s1[i];
-                 }
-                    
-            }
-            cout<<endl;
-            }
-        
-     
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) 
+	{
+        string str;
+        cin >> str;
+
+        for (int j = 0; j < str.length(); j++) 
+		{
+            if (j % 2 == 0)
+            {
+            	cout << str[j];
+			}
+        }
+
+        cout << " ";
+
+        for (int j = 0; j < str.length(); j++) 
+		{
+            if (j % 2 != 0)
+            {
+            	cout << str[j];
+			}
+        }
+
+        cout << endl;
+    }
+
     return 0;
 }
