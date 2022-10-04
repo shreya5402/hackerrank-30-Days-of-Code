02162cb70f2717a72d3fd2beea752be298d40ef9
Shreya Pandey <74672603+shreya5402@users.noreply.github.com>
2022-10-04 15:37:42 +0530

    Update Day6.cpp

--- Day6.cpp
+++ Day6.cpp
@@ -11,31 +11,19 @@ int main() {
     int N;
     cin >> N;
 
-    for (int i = 0; i < N; i++) 
-	{
+    for (int i = 0; i < N; i++){
         string str;
         cin >> str;
 
-        for (int j = 0; j < str.length(); j++) 
-		{
-            if (j % 2 == 0)
-            {
-            	cout << str[j];
-			}
+        for (int j = 0; j < str.length(); j++) {
+            if (j % 2 == 0){
+            	cout << str[j];}
         }
-
         cout << " ";
-
-        for (int j = 0; j < str.length(); j++) 
-		{
-            if (j % 2 != 0)
-            {
-            	cout << str[j];
-			}
+        for (int j = 0; j < str.length(); j++) {
+            if (j % 2 != 0){
+            	cout << str[j];}
         }
-
-        cout << endl;
-    }
-
+        cout << endl;}
     return 0;
 }
